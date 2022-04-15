21e103ad65abc5e5d7a617f34d74a1784411173e
Arbit3r <rikukoski2@gmail.com>
2022-04-15 15:02:13 +0300

    Yleinen päivitys

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,9 @@
 let app = new Vue({
   el: '#app',
   data: {
-    message: 'Mr Giga Lich'
+    message: 'Mr Giga Lich',
+    email: 'giga.chad@meme.fi',
+    phone: '044 1245 678',
+    city: 'Espoo'
   }
 })
