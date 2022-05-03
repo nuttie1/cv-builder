1e2a723331a31105accbdd99cdde375f00d9dbde
Arbit3r <rikukoski2@gmail.com>
2022-05-03 12:37:20 +0300

    pls

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -5,8 +5,18 @@
 let app = new Vue({
   el: '#app',
   data: {
-    message: 'Mr Giga Lich', email: 'giga.chad@meme.fi', phone: '044 1245 678', city: 'Espoo',
-    red: '150', green: '150', blue: '150',
-    colour: {red}
+    message: 'Mr Giga Lich', email: 'giga.chad@meme.fi', phone: '044 1245 678',
+    city: 'Espoo', color: '#000000',
+    items: [
+      { Name: 'Prisma, Iso-Omena', time: '2018 - 2020', type: 'Hyllyttäjä',
+        text: 'Olin kaksi vuotta prismalla töiussä.' +
+            ' In aliquet sapien aliquam lacus vulputate fringilla. In cursus sodales augue a mattis. ' +
+            'Aliquam consectetur mauris nec leo blandit, ut gravida massa imperdiet. In eget arcu id enim mattis ultrices. Mauris venenatis luctus luctus.'},
+      { Name: 'Prisma, Iso-Omena', time: '2018 - 2020', type: 'Hyllyttäjä',
+        text: 'Olin kaksi vuotta prismalla töiussä.' +
+            ' In aliquet sapien aliquam lacus vulputate fringilla. In cursus sodales augue a mattis. ' +
+            'Aliquam consectetur mauris nec leo blandit, ut gravida massa imperdiet. In eget arcu id enim mattis ultrices. Mauris venenatis luctus luctus.'}
+    ]
   }
-})
+});
+//document.documentElement.style.setProperty('--border', 'red');
